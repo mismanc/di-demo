39781d11b281be0c3d49d052ecb51da15af625cb
misman <misman@ubuntuc>
2019-02-10 17:23:53 +0300

    qualifier

--- src/main/java/com/misman/didemo/services/GreetingServiceImpl.java
+++ src/main/java/com/misman/didemo/services/GreetingServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
-	public static final String HELLO_ALL = "Es-Selam to All";
+	public static final String HELLO_ALL = "Es-Selam To All";
 	
 	@Override
 	public String sayGreeting() {
