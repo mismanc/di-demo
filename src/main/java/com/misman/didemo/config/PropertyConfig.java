a47ee0ce21efe9f4f16aec1659a65e0dd08891b8
misman <mismancelaleddin@gmail.com>
2019-02-21 18:20:29 +0300

    ASDAS

--- src/main/java/com/misman/didemo/config/PropertyConfig.java
+++ src/main/java/com/misman/didemo/config/PropertyConfig.java
@@ -1,17 +1,23 @@
 package com.misman.didemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import com.misman.didemo.examplebeans.FakeDataSource;
+import com.misman.didemo.examplebeans.FakeJMSBroker;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
+	@Autowired
+	private Environment env;
+	
 	@Value("${misman.username}")
 	String user;
 	
@@ -21,13 +27,31 @@ public class PropertyConfig {
 	@Value("${misman.dburl}")
 	String dburl;
 	
+	@Value("${jms.username}")
+	String jmsUser;
+	
+	@Value("${jms.password}")
+	String jmsPassword;
+	
+	@Value("${jms.url}")
+	String jmsUrl;
+	
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fk = new FakeDataSource();
 		fk.setPassword(password);
 		fk.setUser(user);
-		fk.setUrl(dburl);
+		fk.setUrl(env.getProperty("MISMAN_CONF"));
 		
+		return fk;
+	}
+	
+	@Bean
+	public FakeJMSBroker fakeJMSBroker() {
+		FakeJMSBroker fk = new FakeJMSBroker();
+		fk.setPassword(jmsPassword);
+		fk.setUser(jmsUser);
+		fk.setUrl(jmsUrl);
 		return fk;
 	}
 	
